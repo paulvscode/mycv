054999c3728bda6b0bcec0fd4180ea78aef52b18
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-05 12:42:08 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>Page</div>;
+  return (
+    <div className='App'>
+      <div className='navbar'>
+        <h4>Paul Barraud de Lagerie</h4>
+        <ul>
+          <li>Réalisations</li>
+          <li>Cv en ligne</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
