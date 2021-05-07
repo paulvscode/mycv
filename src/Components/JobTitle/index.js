46a19d6388896dfa4d0571a89ccfe5857fe0bb22
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-07 22:25:38 +0200

    added description poste

--- src/Components/JobTitle/index.js
+++ src/Components/JobTitle/index.js
@@ -4,7 +4,26 @@ import "./styles.css";
 const JobTitle = () => {
   return (
     <div className='container-jobtitle'>
-      <div className='content-jobtitle'>Développeur FullStack Javascript</div>
+      <div className='content-jobtitle'>
+        <h1> Je suis un développeur Javascript fullstack.</h1>
+        <p>
+          Je suis spécialisé en React pour le front-end et en Node Js pour le
+          back-end.
+        </p>
+        <p>
+          Suite à ma formation chez{" "}
+          <a
+            target='_blank'
+            rel='noreferrer'
+            className='oclock'
+            href='https://oclock.io/'
+          >
+            O'clock
+          </a>
+          , je suis à la recherche d'un poste de développeur fullstack junior
+          dans la région du Havre ou en non-présentiel.
+        </p>
+      </div>
     </div>
   );
 };
