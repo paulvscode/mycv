0c861703d7f01d46134ea689da4279dd7ad59972
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-07 11:09:24 +0200

    fixed background color

--- src/App.js
+++ src/App.js
@@ -9,56 +9,58 @@ import Footer from "./Components/Footer/index";
 
 function App() {
   return (
-    <div className='App'>
-      <div className='container-grid-contain'>
-        <nav className='grid-container-navbar'>
-          <div className='navbar-name'>Paul Barraud de Lagerie</div>
-          <Link
-            activeClass='active'
-            to='realisations'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            <div className='navbar-realisation'>Réalisations</div>
-          </Link>
-          <Link
-            activeClass='active'
-            to='cv'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            <div className='navbar-cvenligne'>Cv en ligne</div>
-          </Link>
-          <Link
-            activeClass='active'
-            to='contact'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            <div className='navbar-contact'>Contact</div>
-          </Link>
-        </nav>
-      </div>
+    <div className='container-background'>
+      <div className='App'>
+        <div className='container-grid-contain'>
+          <nav className='grid-container-navbar'>
+            <div className='navbar-name'>Paul Barraud de Lagerie</div>
+            <Link
+              activeClass='active'
+              to='realisations'
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              <div className='navbar-realisation'>Réalisations</div>
+            </Link>
+            <Link
+              activeClass='active'
+              to='cv'
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              <div className='navbar-cvenligne'>Cv en ligne</div>
+            </Link>
+            <Link
+              activeClass='active'
+              to='contact'
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              <div className='navbar-contact'>Contact</div>
+            </Link>
+          </nav>
+        </div>
 
-      <div className='container-content'>
-        <JobTitle />
-        <div id='realisations'>
-          <Realisations />
+        <div className='container-content'>
+          <JobTitle />
+          <div id='realisations'>
+            <Realisations />
+          </div>
+          <div id='cv'>
+            <CvEnLigne />
+          </div>
+          <div id='contact'>
+            <Contact />
+          </div>
         </div>
-        <div id='cv'>
-          <CvEnLigne />
-        </div>
-        <div id='contact'>
-          <Contact />
-        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
--- src/Components/JobTitle/index.js
+++ src/Components/JobTitle/index.js
@@ -4,7 +4,6 @@ import "./styles.css";
 const JobTitle = () => {
   return (
     <div className='container-jobtitle'>
-      <div className='positionning-div-jobtitle'>Job title</div>
       <div className='content-jobtitle'>Contenu introduction</div>
     </div>
   );
