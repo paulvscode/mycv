2afe55135eef0f6161ad6bd85c81623731874481
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-09 19:22:56 +0200

    added hover on realisations

--- src/Components/Realisations/index.js
+++ src/Components/Realisations/index.js
@@ -8,21 +8,18 @@ const Realisations = () => {
       <div className='positionning-div-realisations'>Réalisations</div>
       <div className='content-realisations'>
         <div className='realisation-box'>
-          <img className='dn-image' src={Dn} alt='' />
+          <div className='container-hover'>
+            <img className='resize-image' src={Dn} alt='' />
+            <div className='color-me'></div>
+          </div>
         </div>
-        <div className='realisation-box'>
-          <img
-            classname='dn-image'
-            src='https://picsum.photos/800/400'
-            alt=''
-          />
-        </div>
-        <div className='realisation-box'>Réalisation 3</div>
-        <div className='realisation-box'>Réalisation 4</div>
-        <div className='realisation-box'>Réalisation 5</div>
-        <div className='realisation-box'>Réalisation 6</div>
-        <div className='realisation-box'>Réalisation 7</div>
-        <div className='realisation-box'>Réalisation 8</div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
+        <div className='realisation-box'></div>
       </div>
     </div>
   );
