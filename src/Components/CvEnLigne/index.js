8b159121bc04e948bc0cd3ee33e9682053528cfa
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-10 11:31:58 +0200

    added cv en ligne content

--- src/Components/CvEnLigne/index.js
+++ src/Components/CvEnLigne/index.js
@@ -5,7 +5,56 @@ const CvEnLigne = () => {
   return (
     <div className='container-cv'>
       <div className='positionning-div-cv'>Cv en ligne</div>
-      <div className='content-cv'>Contenu Cv en ligne</div>
+      <div className='content-cv'>
+        <div className='experience'>
+          <div className='cvenligne-title'>Expérience</div>
+          <div className='cvenligne-description'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+              neque exercitationem mollitia tempore consequuntur sunt ipsa dolor
+              culpa accusantium. Quod!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+              repellendus vitae obcaecati nulla molestias consequatur sequi
+              iure? Architecto corrupti earum natus temporibus, officia,
+              voluptatibus, doloremque quidem deserunt est illo cumque.
+            </p>
+          </div>
+        </div>
+        <div className='formation'>
+          <div className='cvenligne-title'>Formation</div>
+          <div className='cvenligne-description'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+              neque exercitationem mollitia tempore consequuntur sunt ipsa dolor
+              culpa accusantium. Quod!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+              repellendus vitae obcaecati nulla molestias consequatur sequi
+              iure? Architecto corrupti earum natus temporibus, officia,
+              voluptatibus, doloremque quidem deserunt est illo cumque.
+            </p>
+          </div>
+        </div>
+        <div className='soft-skills'>
+          <div className='cvenligne-title'>Soft Skills</div>
+          <div className='cvenligne-description'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+              neque exercitationem mollitia tempore consequuntur sunt ipsa dolor
+              culpa accusantium. Quod!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+              repellendus vitae obcaecati nulla molestias consequatur sequi
+              iure? Architecto corrupti earum natus temporibus, officia,
+              voluptatibus, doloremque quidem deserunt est illo cumque.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
