10a14e01809a90697cc8ec1ed29ee5662959f8f8
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-10 19:15:07 +0200

    added email sending

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,59 +1,48 @@
 import React, { useState } from "react";
-import { useInput } from "../../hook/input-hook/index";
+// import { useInput } from "../../hook/input-hook/index";
 import "./styles.css";
+import { init, sendForm } from "emailjs-com";
+import { useForm } from "react-hook-form";
+const onSubmit = (data) => console.log(data);
+
+init("user_0CrOptuBnm7Nb076RUCzg");
 
 const Contact = () => {
-  const {
-    value: firstName,
-    bind: bindFirstName,
-    reset: resetFirstName,
-  } = useInput("");
+  const { register, handleSubmit, watch, errors } = useForm();
 
-  const { value: email, bind: bindEmail, reset: resetEmail } = useInput("");
-
-  const { value: message, bind: bindMessage, reset: resetMessage } = useInput(
-    ""
-  );
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(`Submitting Name ${firstName}`);
-    console.log(`Submitting Email ${email}`);
-    console.log(`Submitting Message ${message}`);
-    resetFirstName();
-    resetEmail();
-    resetMessage();
+  const onSubmit = (data) => {
+    sendForm("default_service", "template_rwzn49c", "#contact-form").then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
   };
 
   return (
     <div className='container-contact'>
       <div className='positionning-div-contact'>Contact</div>
       <div className='content-contact'>
-        <form id='contact-form' onSubmit={handleSubmit} method='POST'>
-          <div className='form-group'>
-            <label htmlFor='name'>Name</label>
-            <input type='text' {...bindFirstName} className='form-control' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='exampleInputEmail1'>Email address</label>
-            <input
-              {...bindEmail}
-              type='email'
-              className='form-control'
-              aria-describedby='emailHelp'
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='message'>Message</label>
-            <textarea
-              {...bindMessage}
-              className='form-control'
-              rows='5'
-            ></textarea>
-          </div>
-          <button type='submit' className='button-submit'>
-            Submit
-          </button>
+        <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type='text'
+            name='user_name'
+            ref={register}
+            placeholder='Name'
+          />
+          <br />
+          <input
+            type='email'
+            name='user_email'
+            ref={register}
+            placeholder='Email'
+          />
+          <br />
+          <textarea name='message' ref={register} placeholder='Message' />
+          <br />
+          <input type='submit' value='Send' />
         </form>
       </div>
     </div>
