5cabe30a1b9858af2743f2a55cd347c1119357d8
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-11 10:58:07 +0200

    styled footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import LinkedIn from "../../assets/images/174857.png";
 import "./styles.css";
 
 const Footer = () => {
-  return <div className='container-footer'>Footer component</div>;
+  return (
+    <div className='container-footer'>
+      <p className='copyright'>Copyright Paul Barraud de Lagerie - 2021</p>
+      <a
+        rel='noreferrer'
+        target='_blank'
+        href='https://www.linkedin.com/in/paulbarrauddelagerie/'
+      >
+        <img className='linkedin-logo' src={LinkedIn} alt='Linked in' />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
